60c5df2f81688082c9f0da117897773a6818dd26
ZaneWharton <zanewharton@gmail.com>
2025-07-25 16:38:01 -0500

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+#Run server: uvicorn main:app --reload
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "App is Running"}
---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold text-blue-700">
+        App is Running!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
